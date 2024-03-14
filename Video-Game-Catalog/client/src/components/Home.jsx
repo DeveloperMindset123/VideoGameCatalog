66444e46be02b7c76fc3a6be14a996c71b2c7560
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-03-14 13:14:54 -0400

    fixed tailwindcss not rendering due to config files, changed extensions to .cjs from .jsto utilize commonjs module

--- Video-Game-Catalog/client/src/components/Home.jsx
+++ Video-Game-Catalog/client/src/components/Home.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { NavbarWithSearch } from './NavbarWithSearch'
+
 
 export const Home = () => {
   return (
-    <div><h1>Test Home Page</h1></div>
+    <div><NavbarWithSearch /></div>
   )
 }
 
